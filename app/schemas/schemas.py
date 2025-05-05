34eaba8eb33f5e00b3ea0955b85b1ced12a5b32f
VRFounder <vr.solutions.school@gmail.com>
2025-05-06 00:20:04 +0500

    all changes

---
+++ app/schemas/schemas.py
@@ -0,0 +1,71 @@
+from pydantic import BaseModel, EmailStr
+from typing import List, Optional, Any
+from uuid import UUID
+from datetime import datetime
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    access: List[str] = []
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    access: List[str] = []
+
+
+class UserRead(BaseModel):
+    id: UUID
+    username: str
+    access: List[str]
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class TourCreate(BaseModel):
+    name_ru: str
+    name_en: str
+    countries: List[str]
+    duration: int
+    dates: List[str]
+    description_ru: str
+    description_en: str
+    price: int
+    extra_costs_ru: List[dict] = []
+    extra_costs_en: List[dict] = []
+    accommodation: List[str]
+    category: str
+    tags: List[str]
+    routes: List[Any]
+
+
+class TourRead(TourCreate):
+    id: UUID
+    image_path: Optional[str]
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class OrderCreate(BaseModel):
+    tour: UUID
+    citizenship: str
+    phone: str
+    tg: Optional[str]
+    email: EmailStr
+    date: str
+    user_count: int
+    comment: Optional[str] = ""
+
+
+class OrderRead(OrderCreate):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
